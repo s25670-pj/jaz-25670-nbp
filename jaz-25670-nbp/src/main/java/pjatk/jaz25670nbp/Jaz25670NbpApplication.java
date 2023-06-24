cd12f4309d2b9f448d5366bf602645b16604755b
s25670-pj <92204700+s25670-pj@users.noreply.github.com>
2023-06-24 12:48:15 +0200

    adding files

---
+++ jaz-25670-nbp/src/main/java/pjatk/jaz25670nbp/Jaz25670NbpApplication.java
@@ -0,0 +1,13 @@
+package pjatk.jaz25670nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz25670NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaz25670NbpApplication.class, args);
+    }
+
+}
---
+++ jaz-25670-nbp/src/test/java/pjatk/jaz25670nbp/Jaz25670NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.jaz25670nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz25670NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
